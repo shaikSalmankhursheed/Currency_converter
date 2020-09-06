b06f3aa010af3f983baf112abb246605f2410c73
Shaik Salman Khursheed <45688914+shaikSalmankhursheed@users.noreply.github.com>
2020-09-07 03:22:19 +0530

    Second commit
    Added mock APIs for converting and completed the app

--- src/App.js
+++ src/App.js
@@ -15,33 +15,60 @@ import './App.css';
 const App = () => {
 
     const [curList, SetCurList] = useState([])
-    const [data, Setdata] = useState({amount: "", From: "", To: ""})
+    const [data1, Setdata1] = useState({amount: "", From: "", To: ""})
+    const [result, Setresult] = useState(null)
 
     const handleSubmit = () => {
-       if ( data.amount == "" || data.From== "" || data.To == ""){
-         alert("Please input all your values")
-       }
-
-       else{
-         alert("all values entered")
-       }
+        if (data1.amount == "" || data1.From == "" || data1.To == "") {
+            alert("Please input all your values")
+        } else if (data1.From == data1.To) {
+            alert("Choose a different currency combination")
+        } else {
+            CallConverterapi(data1)
+        }
     }
 
 
-    useEffect(()=>{
-      axios
-        .get(`http://demo4088948.mockable.io/all_currencies`).then(function (response) {
-          
-           console.log(response.data);
-          SetCurList(response.data);
-          
-        })
-        .catch(function (error) {
-          // handle error
-          console.log(error);
+    useEffect(() => {
+        axios.get(`http://demo4088948.mockable.io/all_currencies`).then(function (response) {
+
+            console.log(response.data);
+            SetCurList(response.data);
+
+        }).catch(function (error) { // handle error
+            console.log(error);
         })
 
-    },[])
+    }, [])
+
+
+    const updateResult = (x) => {
+
+
+        Setresult(parseFloat(x[data1.To] / x[data1.From] * data1.amount).toFixed(2));
+
+        // let x = data1.From
+        // let y = data1.To
+
+        // console.log (`response.data.rates.${x}`)
+        // console.log((  `response.data.rates.${x}` / `response.data.rates.${y}` ) * data.amount)
+    }
+    const CallConverterapi = (data1) => {
+
+        axios.get(` http://data.fixer.io/api/latest?access_key=899192805559fb4fce628932a976a574&symbols=${
+            data1.From
+        },${
+            data1.To
+        }&format=1`).then(function (response) {
+
+            updateResult(response.data.rates)
+
+
+        }).catch(function (error) { // handle error
+            console.log(error);
+        })
+
+    }
 
     return (
         <div className="App">
@@ -61,8 +88,8 @@ const App = () => {
                 <div className="row justify-content-center align-items-center">
                     <div className="col-md-4"><input type="number" min="0"
                             onChange={
-                                (e) => Setdata({
-                                    ...data,
+                                (e) => Setdata1({
+                                    ...data1,
                                     amount: e.target.value
                                 })
                             }/></div>
@@ -77,19 +104,21 @@ const App = () => {
                 <div className="row justify-content-center align-items-center">
                     <div className="col-md-4">
 
-                        <select className="custom-select" id="inputGroupSelect01 " defaultValue={''}
+                        <select className="custom-select" id="inputGroupSelect01 "
+                            defaultValue={''}
                             onChange={
                                 (e) => {
-                                    Setdata({
-                                        ...data,
+                                    Setdata1({
+                                        ...data1,
                                         From: e.target.value
                                     })
                                 }
                         }>
-                            <option value=""  disabled>Choose Currency</option>
+                            <option value="" disabled>Choose Currency</option>
                             {
-                            curList.map((item,index) => {
-                                return <option key={index} value={item}>
+                            curList.map((item, index) => {
+                                return <option key={index}
+                                    value={item}>
                                     {item}</option>
                         })
                         } </select>
@@ -108,24 +137,51 @@ const App = () => {
                     <div className="col-md-4">
 
 
-                        <select className="custom-select" id="inputGroupSelect01" defaultValue={''}
+                        <select className="custom-select" id="inputGroupSelect01"
+                            defaultValue={''}
                             onChange={
-                                e => Setdata({
-                                    ...data,
+                                e => Setdata1({
+                                    ...data1,
                                     To: e.target.value
                                 })
                         }>
-                            <option value=""  disabled>Choose Currency</option>
+                            <option value="" disabled>Choose Currency</option>
                             {
-                            curList.map((item,index) => {
-                                return <option key={index} value={item}>
+                            curList.map((item, index) => {
+                                return <option key={index}
+                                    value={item}>
                                     {item}</option>
                         })
                         } </select>
                         <h1></h1>
-                        <div className="btn btn-primary"
-                            onClick={handleSubmit}>Convert NOW</div>
-                    </div>
+                        {
+                        result ? (
+                            <p>Converting {
+                                data1.amount
+                            }
+                                {
+                                data1.From
+                            }
+                                to {
+                                data1.To
+                            }
+                                is
+                                <h1>{result}</h1>
+                                {
+                                data1.To
+                            } </p>
+                        ) : ("")
+                    }
+
+                        {
+                        result ? (
+                            <div className="btn btn-danger" onClick= { ()=>{window.location.reload();}}>
+                                Reset conversion</div>
+                        ) : (
+                            <div className="btn btn-primary"
+                                onClick={handleSubmit}>Convert NOW</div>
+                        )
+                    } </div>
 
 
                     {' '} </div>
